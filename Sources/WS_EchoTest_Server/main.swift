a849c8521b6c9e6e5d67b1355dc30af363df7f84
luca69cr <57489410+luca69cr@users.noreply.github.com>
2020-02-04 04:53:16 +0100

    aa

---
+++ Sources/WS_EchoTest_Server/main.swift
@@ -0,0 +1,30 @@
+//
+//  UploadHandler.swift
+//  Upload Enumerator
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+do {
+    // Launch the HTTP server on port 8181
+    //try HTTPServer.launch(name: "websockets server", port: 8181, routes: makeRoutes(), responseFilters: [(try HTTPFilter.contentCompression(data: [:]),.high)])
+    try HTTPServer.launch(name: "websockets server", port: 8181, routes: makeRoutes())
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
